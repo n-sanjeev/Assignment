fdffc7bb66d953d10136490c2a67908444d21df6
n-sanjeev <nongmaithemsanjeev@gmail.com>
2022-02-01 17:04:56 +0530

    added FizzBUzz in one line

---
+++ FizzBuzz_InOneLine.js
@@ -0,0 +1,16 @@
+
+
+let str = "";
+for (let i = 1; i<=100; i++) {
+    switch (true) {
+        case i % 3 === 0 && i % 5 === 0:
+            str += "FizzBuzz ";
+        case i % 3 === 0:
+            str += " Fizz";
+        case i % 5 === 0:
+            str += "Buzz ";
+        default:
+            str += i + " ";
+            str;
+    }
+    }
